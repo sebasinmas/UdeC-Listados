7a7f6ef48881f3a55f156d4fc0d3d2bf43aec49a
SebaSinMas <sebastianbustos90@gmail.com>
2022-04-11 22:45:36 -0400

    Solucionadas las coordenadas

--- primerListC/e9.c
+++ primerListC/e9.c
@@ -7,11 +7,25 @@
 //El código en todos los casos es mío a menos que especifique lo contrario >:( -Sebastián SinMas
 
 int main(){
-int cord1,cord2,cord3;
-scanf("%d %d %d",&cord1,&cord2,&cord3);
+int cord1x,cord1y,cord2x,cord2y,cord3x,cord3y;
+printf("Por favor escriba la primera coordenada X e Y: ");
+scanf("%d %d", &cord1x,&cord1y);
+printf("Por favor escriba la segunda coordenada X e Y: ");
+scanf("%d %d", &cord2x, &cord2y);
+printf("Por favor escriba la tercera coordenada X e Y: ");
+scanf("%d %d", &cord3x, &cord3y);
 
-
-
+if((cord3x>=0 && cord3y>=0) && cord3x <= cord2x && cord3y <=cord2y || (cord3x>=0 && cord3y>=0) && cord3x <= cord1x && cord3y <=cord1y ){
+    printf("\ncontenido");    
+}else if((cord3x<=0 && cord3y>=0) && cord3x >= cord2x && cord3y <=cord2y || (cord3x<=0 && cord3y>=0) && cord3x >= cord1x && cord3y <=cord1y){
+    printf("\ncontenido"); 
+}else if((cord3x<=0 && cord3y<=0) && cord3x >= cord2x && cord3y >=cord2y || (cord3x<=0 && cord3y<=0) && cord3x >= cord1x && cord3y >=cord1y){
+    printf("\ncontenido");
+}else if((cord3x>=0 && cord3y<=0) && cord3x <= cord2x && cord3y >=cord2y || (cord3x>=0 && cord3y<=0) && cord3x <= cord1x && cord3y >=cord1y){
+    printf("\ncontenido");
+}else{
+    printf("\nno contenido");
+}
 
 
 
