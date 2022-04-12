fc04a77060128c19be2f6004048f50034a1d3562
SebaSinMas <sebastianbustos90@gmail.com>
2022-04-11 23:45:33 -0400

    Ejercicio de Timus

--- primerListC/e10.c
+++ primerListC/e10.c
@@ -1,21 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-// 
-//
-//
-
+/*
+https://acm.timus.ru/problem.aspx?space=1&num=1409
+*/
+//El código en todos los casos es mío a menos que especifique lo contrario >:( -Sebastián SinMas
 int main(){
-
-
-
-
-
-
-
-
-
-
-
+	int harry,larry;
+ 	scanf("%d %d",&harry,&larry);
+	printf("%d %d",larry-1,harry-1);
 	return (0);
 }
