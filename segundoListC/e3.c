c2fae4e294d2b608557bfa4e57feedf5b69cd904
SebaSinMas <sebastianbustos90@gmail.com>
2022-05-01 19:22:56 -0400

    Me demoré caleta jajaja

--- segundoListC/e3.c
+++ segundoListC/e3.c
@@ -9,7 +9,45 @@ imprima: el menor, el mayor, el promedio (entero) y la mediana. Valide que n sea
 
 int main(){
 	int n;
+	printf("Cuantos numeros quiere ingresar?: ");
+	scanf("%d",&n);
+	int nums[n];
+	for (int i = 0; i < n; ++i){
+		scanf("%d",&nums[i]);
+	}
+	int menor = nums[0];
+	int mayor = nums[0];
 
-
+	//Busco el menor
+	for (int i = 0; i < n; ++i){
+		if(menor>nums[i]){
+			menor=nums[i];
+		}
+	}
+	//Busco el mayor
+	for (int i = 0; i < n; ++i){
+		if(mayor<nums[i]){
+			mayor=nums[i];
+		}
+	}
+	//calculo el promedio
+	int promedioSinDividir=0;
+	for (int i = 0; i < n; ++i){
+		promedioSinDividir+=nums[i];
+	}
+	//calculo la mediana
+	//este pedazo de código lo saqué de internet(http://codigogx.blogspot.com/2016/07/como-calcular-la-mediana-en-c-java.html)
+	//porque no quería escribir el ordenamiento de burbuja
+	int aux=0;
+	 for(int i = 0; i < n; i++){
+    	for(int j = 0; j < n; j++){
+        	if(nums[j] > nums[j + 1]){
+            	aux = nums[j];
+            	nums[j] = nums[j + 1];
+            	nums[j + 1] = aux;
+        	}
+    	}
+  	}
+	printf("El menor es: %d  El mayor es: %d  La mediana es: %d  El promedio es: %d \n",menor,mayor, nums[(n+1)/2] ,promedioSinDividir/n);
 	return (0);
 }
